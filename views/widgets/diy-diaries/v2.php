64905ce4464631ab3b9564e10ce06d63c937fa02
Ken <KenIovino@gmail.com>
2014-02-06 10:55:18 -0500

    Fixed missing ad URL

--- views/widgets/diy-diaries/v2.php
+++ views/widgets/diy-diaries/v2.php
@@ -16,11 +16,11 @@
             <!-- Womensforum/widget -->
             <script type="text/javascript">
                 var ord = window.ord || Math.floor(Math.random() * 1e16);
-                document.write('<script type="text/javascript" src="=300x250;ord=' + ord + '?"><\/script>');
+                document.write('<script type="text/javascript" src="http://ad.doubleclick.net/N5809/adj/5089.wfm/widget;sz=300x250;ord=' + ord + '?"><\/script>');
             </script>
             <noscript>
-                <a href="=300x250;ord=[timestamp]?">
-                    <img src="=300x250;ord=[timestamp]?" width="300" height="250" />
+                <a href="http://ad.doubleclick.net/N5809/jump/5089.wfm/widget;sz=300x250;ord=[timestamp]?">
+                    <img src="http://ad.doubleclick.net/N5809/ad/5089.wfm/widget;sz=300x250;ord=[timestamp]?" width="300" height="250" />
                 </a>
             </noscript>
 		</div>

6902a96c3b3a1715c8cddfc1d8065c278e5dda63
Ken <KenIovino@gmail.com>
2014-02-10 15:49:04 -0500

    Spring fashion widget

---
+++ views/examples/spring-fashion-v1.php
@@ -0,0 +1,4 @@
+<!-- Womensforum.com - Spring Fashion v1 -->
+<script type="text/javascript" src="http://<?php echo $_SERVER[HTTP_HOST] ?>/v2/widget.min.js"></script>
+<div id="wf_widget"></div>
+<script>com.womensforum.widgets.load("99fbc3bd2172381c56335a0dfb12dc84", "wf_widget");</script>
---
+++ views/widgets/spring-fashion/v1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php echo $this->helper->renderCss("main"); ?>
+	<?php echo $this->helper->renderCss("spring-fashion"); ?>
+</head>
+<body>
+	<div id="wf_widget_countdown">
+		<div id="wf_widget_header">
+			<div id="wf_widget_by">Brought to you by <span>Womensforum.com </span></div>
+		</div>
+		<div id="wf_widget_content">
+			Spring Fashion top
+		</div>
+		<div id="wf_widget_footer">
+			Spring Fashion bot
+		</div>
+	</div>
+</body>
+</html>
+

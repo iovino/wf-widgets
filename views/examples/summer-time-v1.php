3223eee5074aeaf5f797ef3a519a35073a1f25f6
Ken <KenIovino@gmail.com>
2014-03-11 11:23:17 -0400

    Summertime Widget

---
+++ views/examples/summer-time-v1.php
@@ -0,0 +1,4 @@
+<!-- Womensforum.com - Summer Time v1 -->
+<script type="text/javascript" src="http://<?php echo $_SERVER[HTTP_HOST] ?>/v2/widget.min.js"></script>
+<div id="wf_widget"></div>
+<script>com.womensforum.widgets.load("37e8cc58edfe858aeb92fb58879fafa0", "wf_widget");</script>
---
+++ views/widgets/summer-time/v1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php echo $this->helper->renderCss("main"); ?>
+	<?php echo $this->helper->renderCss("summer-time"); ?>
+</head>
+<body>
+	<div id="wf_widget_countdown">
+		<div id="wf_widget_header">
+			<div id="wf_widget_by">Brought to you by <span>Womensforum.com </span></div>
+		</div>
+		<div id="wf_widget_content">
+
+            <ul>
+                <li class="alt1">
+                    <img src="/assets/imgs/summer-time/surviving-the-summer-heat.jpg" width="60" height="60" class="pull-left" />
+                    <a  href="http://www.womensforum.com/surviving-the-summer-heat.html" target="_blank">Surviving the Summer Heat</a>
+                </li>
+                <li class="alt2">
+                    <img src="/assets/imgs/summer-time/hot-summer-make-up-trends.jpg" width="60" height="60" class="pull-left" />
+                    <a  class="single" href="http://www.womensforum.com/hot-summer-make-up-trends.html" target="_blank">Hot Summer Makeup Trends</a>
+                </li>
+                <li class="alt3">
+                    <img src="/assets/imgs/summer-time/must-haves-for-healthy-summer-hair-and-skin-video.jpg" width="60" height="60" class="pull-left" />
+                    <a  href="http://www.womensforum.com/must-haves-for-healthy-summer-hair-and-skin-video.html" target="_blank">Must-Haves for Healthy Summer Hair and Skin</a>
+                </li>
+            </ul>
+
+
+		</div>
+		<div id="wf_widget_footer">
+			<a href="http://www.coppertone.com/">
+                <img src="/assets/imgs/summer-time/coppertone-ad.jpg" border="0" />
+			</a>
+		</div>
+	</div>
+</body>
+</html>
+

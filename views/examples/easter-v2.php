aa3994c676eb2dc69c53185d631a079ed6e12421
Ken <KenIovino@gmail.com>
2014-03-18 11:17:30 -0400

    Easter Widget v2 With Hershey's Ad

---
+++ views/examples/easter-v2.php
@@ -0,0 +1,4 @@
+<!-- Womensforum.com - Easter v1 -->
+<script type="text/javascript" src="http://<?php echo $_SERVER[HTTP_HOST] ?>/v2/widget.min.js"></script>
+<div id="wf_widget"></div>
+<script>com.womensforum.widgets.load("e25307f3003fa6905c8737ec910e1141", "wf_widget");</script>
---
+++ views/widgets/easter/v2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php echo $this->helper->renderCss("main"); ?>
+    <?php echo $this->helper->renderCss("easter"); ?>
+</head>
+<body>
+<div id="wf_holiday_countdown">
+    <div id="wf_widget_header">
+        <div id="wf_widget_by">Brought to you by <span>Womensforum.com</span></div>
+    </div>
+    <div id="wf_holiday_content">
+        <?php echo $this->helper->fetchChannel('easter-passover.html'); ?>
+    </div>
+    <div id="wf_holiday_footer">
+        <a href="http://www.hershey.com/" target="_blank"><img src="/assets/imgs/easter/hershey-ad.png" border="0" /></a>
+    </div>
+</div>
+</body>
+</html>
+

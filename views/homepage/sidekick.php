a551dc0e042c7e26a013eab253eba5109be83e0a
Ken <KenIovino@gmail.com>
2014-03-21 12:49:34 -0400

    Removed slide links and placed them on top of each other

--- views/homepage/sidekick.php
+++ views/homepage/sidekick.php
@@ -1,11 +1,6 @@
-<?php if ($_REQUEST['slide'] == 1 || empty($_REQUEST['slide'])) : ?>
 <div style="text-align: center;">
-    <a href="?do=sidekick&slide=2"><img src="/assets/imgs/homepage/WF_SideKick_1.jpg" border="0" /></a>
+    <img src="/assets/imgs/homepage/WF_SideKick_1.jpg" border="0" />
 </div>
-<?php endif ?>
-
-<?php if ($_REQUEST['slide'] == 2) : ?>
-    <div style="text-align: center;">
-        <a href="?do=sidekick&slide=1"><img src="/assets/imgs/homepage/WF_Sidekick2.png" border="0" /></a>
-    </div>
-<?php endif ?>
+<div style="text-align: center;">
+    <img src="/assets/imgs/homepage/WF_Sidekick2.png" border="0" />
+</div>

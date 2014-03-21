d3264eba1dc88cce8f3c5ed3dab9573251b5ce21
Ken <KenIovino@gmail.com>
2014-03-21 13:42:54 -0400

    Removed Image ref in titles

--- views/homepage/sidekick.php
+++ views/homepage/sidekick.php
@@ -1,8 +1,8 @@
 <div class="col-lg-3 text-center">
-    <h3>Collapsed: Image 1</h3>
+    <h3>Collapsed</h3>
     <img src="/assets/imgs/homepage/WF_Sidekick_1.png" border="0" />
 </div>
 <div class="col-lg-9 text-center">
-    <h3>Expanded: Image 2</h3>
+    <h3>Expanded</h3>
     <img src="/assets/imgs/homepage/WF_Sidekick_2.png" border="0" />
 </div>

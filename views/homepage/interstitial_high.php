b125b677e5b230c5a8d6c26ba5ca0eac16c36cfa
Ken <KenIovino@gmail.com>
2014-03-21 17:34:53 -0400

    fixed typo

--- views/homepage/interstitial_high.php
+++ views/homepage/interstitial_high.php
@@ -1,7 +1,7 @@
 <!-- products_interstitial -->
 <script type="text/javascript">
     var ord = window.ord || Math.floor(Math.random() * 1e16);
-    document.write('<script type="text/javascript" src="http://ad.doubleclick.net/N5809/adj/products_interstitial;dcopt=ist;;sz=1x1;ord=' + ord + '?"><\/script>');
+    document.write('<script type="text/javascript" src="http://ad.doubleclick.net/N5809/adj/products_interstitial;dcopt=ist;sz=1x1;ord=' + ord + '?"><\/script>');
 </script>
 <noscript>
     <a href="http://ad.doubleclick.net/N5809/jump/products_interstitial;sz=1x1;ord=[timestamp]?">

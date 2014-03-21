d703f994011fe18ac214c1eca10220dc1f983164
Ken <KenIovino@gmail.com>
2014-03-21 14:54:12 -0400

    Added flim strip page

---
+++ views/homepage/filmstrip.php
@@ -0,0 +1,22 @@
+<div class="col-lg-12 text-center">
+    <div>
+        <h3>Flim Strip 1</h3>
+        <img src="/assets/imgs/homepage/flimstrip1.png" border="0" />
+    </div>
+    <div>
+        <h3>Flim Strip 2</h3>
+        <img src="/assets/imgs/homepage/flimstrip2.png" border="0" />
+    </div>
+    <div>
+        <h3>Flim Strip 3</h3>
+        <img src="/assets/imgs/homepage/flimstrip3.png" border="0" />
+    </div>
+    <div>
+        <h3>Flim Strip 4</h3>
+        <img src="/assets/imgs/homepage/flimstrip4.png" border="0" />
+    </div>
+    <div>
+        <h3>Flim Strip 5</h3>
+        <img src="/assets/imgs/homepage/flimstrip5.png" border="0" />
+    </div>
+</div>
--- views/homepage/index.php
+++ views/homepage/index.php
@@ -8,7 +8,7 @@ $q1_tabs        = array();
 $q2_tabs        = array('easter', 'mothersday', 'fathersday', 'worldcup', 'springfashion');
 $q3_tabs        = array('backtoschool', 'summertime');
 $custom_tabs    = array('hershey', 'coppertone', 'allaway');
-$high_tabs      = array('sidekick');
+$high_tabs      = array('sidekick', 'filmstrip');
 ?>
 
 <!DOCTYPE html>
@@ -111,6 +111,7 @@ $high_tabs      = array('sidekick');
                 <a href="#" id="q3_tab" class="dropdown-toggle" data-toggle="dropdown">High Impact <b class="caret"></b></a>
                 <ul class="dropdown-menu" role="menu" aria-labelledby="q3_tab">
                     <li><a href="?do=sidekick" tabindex="-1">Side Kick</a></li>
+                    <li><a href="?do=filmstrip" tabindex="-1">Film Strip</a></li>
                 </ul>
             </li>
 

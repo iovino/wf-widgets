f62bef89481b19771b6e458fd4909fd3be08139b
Ken <KenIovino@gmail.com>
2014-03-21 13:23:50 -0400

    Updated sidekick page

--- views/homepage/sidekick.php
+++ views/homepage/sidekick.php
@@ -1,8 +1,8 @@
 <div class="col-lg-6 text-center">
     <h3>Collapsed: Image 1</h3>
-    <img src="/assets/imgs/homepage/WF_Sidekick_1.png" border="0" />
+    <img src="/assets/imgs/homepage/WF_Sidekick1.png" border="0" />
 </div>
 <div class="col-lg-6 text-center">
     <h3>Expanded: Image 1</h3>
-    <img src="/assets/imgs/homepage/WF_Sidekick_2.png" border="0" />
+    <img src="/assets/imgs/homepage/WF_Sidekick2.png" border="0" />
 </div>

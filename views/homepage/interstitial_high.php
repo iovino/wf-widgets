f37cc163e3368df1732a46ed87e9f86a40fdd5ca
Ken <KenIovino@gmail.com>
2014-03-21 16:53:51 -0400

    Updated products_interstitial's ad code

--- views/homepage/interstitial_high.php
+++ views/homepage/interstitial_high.php
@@ -1,6 +1,10 @@
+<!-- products_interstitial -->
 <script type="text/javascript">
     var ord = window.ord || Math.floor(Math.random() * 1e16);
-    document.write('<script data-mce-type="text/javascript" src="http://ad.doubleclick.net/N5809/adj/5089.wfm/test_page2;dcopt=ist;sz=1x1;ord=' + ord + '?"><\/script>');
+    document.write('<script type="text/javascript" src="http://ad.doubleclick.net/N5809/adj/products_interstitial;dcopt=ist;;sz=1x1;ord=' + ord + '?"><\/script>');
 </script>
-<noscript><a href="http://ad.doubleclick.net/N5809/jump/5089.wfm/test_page2;dcopt=ist;sz=1x1;ord=[timestamp]"> <img src="http://ad.doubleclick.net/N5809/ad/5089.wfm/test_page2;dcopt=ist;sz=1x1;ord=[timestamp]" width="1" height="1"/> </a></noscript>
-
+<noscript>
+    <a href="http://ad.doubleclick.net/N5809/jump/products_interstitial;sz=1x1;ord=[timestamp]?">
+        <img src="http://ad.doubleclick.net/N5809/ad/products_interstitial;sz=1x1;ord=[timestamp]?" width="1" height="1" />
+    </a>
+</noscript>

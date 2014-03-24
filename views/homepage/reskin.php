ff9b4ad3a46c17c53deb6901a90c3f015f28446f
Ken <KenIovino@gmail.com>
2014-03-24 13:40:45 -0400

    Added reskin

---
+++ views/homepage/reskin.php
@@ -0,0 +1,12 @@
+<div class="col-lg-12 text-center">
+    <h3>Standard</h3>
+    <img src="/assets/imgs/homepage/reskin/WFHomepage_Reskin3.png" border="0" width="1100" />
+</div>
+<div class="col-lg-12 text-center">
+    <h3>Slideshow</h3>
+    <img src="/assets/imgs/homepage/reskin/WFHomepage_Reskin2.png" border="0" width="1100" />
+</div>
+<div class="col-lg-12 text-center">
+    <h3>Video</h3>
+    <img src="/assets/imgs/homepage/reskin/WFHomepage_Reskin1.png" border="0" width="1100" />
+</div>

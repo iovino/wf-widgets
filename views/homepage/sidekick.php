02c8247c807fe66ff7143988d8cdf6d3e9e38eed
Ken <KenIovino@gmail.com>
2014-03-24 14:39:31 -0400

    Updated and added new sidekick ads

--- views/homepage/sidekick.php
+++ views/homepage/sidekick.php
@@ -1,8 +1,19 @@
 <div class="col-lg-3 text-center">
-    <h3>Collapsed</h3>
-    <img src="/assets/imgs/homepage/WF_Sidekick_1.png" border="0" />
+    <h3>Collapsed - 300x250</h3>
+    <img src="/assets/imgs/homepage/sidekick/WF_Sidekick_1.png" border="0" />
 </div>
 <div class="col-lg-9 text-center">
-    <h3>Expanded</h3>
-    <img src="/assets/imgs/homepage/WF_Sidekick_2.png" border="0" />
+    <h3>Expanded - 600x500</h3>
+    <img src="/assets/imgs/homepage/sidekick/WF_Sidekick_2.png" border="0" />
+</div>
+
+<hr>
+
+<div class="col-lg-3 text-center">
+    <h3>Collapsed - 300x600</h3>
+    <img src="/assets/imgs/homepage/sidekick/BacktoSchool_HorizonExpandable_1.png" border="0" />
+</div>
+<div class="col-lg-9 text-center">
+    <h3>Expanded - 600x600</h3>
+    <img src="/assets/imgs/homepage/sidekick/BacktoSchool_HorizonExpandable_2.png" border="0" />
 </div>
--- views/homepage/super-pushdown.php
+++ views/homepage/super-pushdown.php
@@ -1,3 +1,5 @@
+<div class="col-lg-12 text-center"><h2>Style</h2></div>
+
 <div class="col-lg-12 text-center">
     <h3>Lookbook</h3>
     <img src="/assets/imgs/homepage/super-pushdown/lookbook.png" border="0" />
@@ -9,4 +11,19 @@
 <div class="col-lg-12 text-center">
     <h3>Video</h3>
     <img src="/assets/imgs/homepage/super-pushdown/video.png" border="0" />
+</div>
+
+<div class="col-lg-12 text-center"><h2>Food</h2></div>
+
+<div class="col-lg-12 text-center">
+    <h3>Recipes</h3>
+    <img src="/assets/imgs/homepage/super-pushdown/WFFood_SuperPushDown_3.jpg" border="0" />
+</div>
+<div class="col-lg-12 text-center">
+    <h3>Shopping List</h3>
+    <img src="/assets/imgs/homepage/super-pushdown/WFFood_SuperPushDown_2.jpg" border="0" />
+</div>
+<div class="col-lg-12 text-center">
+    <h3>Videos</h3>
+    <img src="/assets/imgs/homepage/super-pushdown/WFFood_SuperPushDown_1.jpg" border="0" />
 </div>

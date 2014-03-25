7b9bd351fb766e32656f83d7bf0bdedd51594b21
Ken <KenIovino@gmail.com>
2014-03-24 21:14:46 -0400

    Updated world cup widgets

---
+++ views/examples/world-cup-v2.php
@@ -0,0 +1,4 @@
+<!-- Womensforum.com - World Cup v1 -->
+<script type="text/javascript" src="http://<?php echo $_SERVER[HTTP_HOST] ?>/v2/widget.min.js"></script>
+<div id="wf_widget"></div>
+<script>com.womensforum.widgets.load("fd268914ea7b4a2a1c6f77ff8c1dcf04", "wf_widget");</script>
--- views/homepage/worldcup.php
+++ views/homepage/worldcup.php
@@ -1,6 +1,13 @@
-<div class="col-lg-12 text-center">
-    <h3>World Cup Video + Pre-roll</h3>
+<div class="col-lg-6 text-center">
+    <h3>World Cup Content w/ Video + Pre-roll</h3>
     <script type="text/javascript" src="<?php echo $app->config('widget_js_url'); ?>"></script>
     <div id="wf_widget_v1"></div>
     <script>com.womensforum.widgets.load("d429483a9817359368c2a3cfa0d95c7b", "wf_widget_v1");</script>
+</div>
+
+<div class="col-lg-6 text-center">
+    <h3>World Cup Content w/ 300x250 Ad</h3>
+    <script type="text/javascript" src="<?php echo $app->config('widget_js_url'); ?>"></script>
+    <div id="wf_widget_v2"></div>
+    <script>com.womensforum.widgets.load("fd268914ea7b4a2a1c6f77ff8c1dcf04", "wf_widget_v2");</script>
 </div>

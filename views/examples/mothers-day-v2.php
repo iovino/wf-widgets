e5d78ce689e2a0fd54c1ab074a6cf6a7aecfd6a8
Ken <KenIovino@gmail.com>
2014-03-24 21:06:09 -0400

    Updated Mother's Day widgets

---
+++ views/examples/mothers-day-v2.php
@@ -0,0 +1,4 @@
+<!-- Womensforum.com - Mothers Day v1 -->
+<script type="text/javascript" src="http://<?php echo $_SERVER[HTTP_HOST] ?>/v2/widget.min.js"></script>
+<div id="wf_widget"></div>
+<script>com.womensforum.widgets.load("9ccb6a40675bc2f0471621af70de0624", "wf_widget");</script>
--- views/homepage/mothersday.php
+++ views/homepage/mothersday.php
@@ -1,6 +1,13 @@
-<div class="col-lg-12 text-center">
-    <h3>Mothers Day Video + Pre-roll</h3>
+<div class="col-lg-6 text-center">
+    <h3>Mothers Day Content w/ Video + Pre-roll</h3>
     <script type="text/javascript" src="<?php echo $app->config('widget_js_url'); ?>"></script>
     <div id="wf_widget_v1"></div>
     <script>com.womensforum.widgets.load("eb9db964914ca9e57646864e5bf71a41", "wf_widget_v1");</script>
+</div>
+
+<div class="col-lg-6 text-center">
+    <h3>Mothers Day Content w/ 300x250 Ad</h3>
+    <script type="text/javascript" src="<?php echo $app->config('widget_js_url'); ?>"></script>
+    <div id="wf_widget_v2"></div>
+    <script>com.womensforum.widgets.load("9ccb6a40675bc2f0471621af70de0624", "wf_widget_v2");</script>
 </div>

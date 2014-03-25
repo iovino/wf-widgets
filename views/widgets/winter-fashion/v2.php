0ec9a25f3cc8a1dcc9713841592db90201833965
Ken <KenIovino@gmail.com>
2014-03-25 16:50:51 -0400

    Winter Fashion Content

--- views/widgets/winter-fashion/v2.php
+++ views/widgets/winter-fashion/v2.php
@@ -10,7 +10,25 @@
 			<div id="wf_widget_by">Brought to you by <span>Womensforum.com </span></div>
 		</div>
 		<div id="wf_widget_content">
-
+            <ul>
+                <li class="alt1">
+                    <img src="/assets/imgs/winter-fashion/Snow_Boots_Done_Cute.jpg" width="60" height="60" class="pull-left" />
+                    <a href="http://www.womensforum.com/how-to-wear-snow-boots.html" target="_blank">
+                        Snow Boots Done Cute
+                    </a>
+                </li>
+                <li class="alt2"><img src="/assets/imgs/winter-fashion/Fresh_New_Ways_to_Wear_a_Turtleneck.jpg" width="60" height="60" class="pull-left" />
+                    <a href="http://www.womensforum.com/fresh-new-ways-to-wear-a-turtleneck.html" target="_blank">
+                        Fresh New Ways to Wear a Turtleneck
+                    </a>
+                </li>
+                <li class="alt3">
+                    <img src="/assets/imgs/winter-fashion/winter_sports_fashion.jpg" width="60" height="60" class="pull-left" />
+                    <a href="http://www.womensforum.com/winter-sports-fashion.html" target="_blank">
+                        Winter Sports Fashion
+                    </a>
+                </li>
+            </ul>
 		</div>
 		<div id="wf_widget_footer">
             <script type="text/javascript">

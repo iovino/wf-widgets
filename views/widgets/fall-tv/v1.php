f47c2faed355496a6815743674a633c855f8bf03
Ken <KenIovino@gmail.com>
2014-03-25 16:47:02 -0400

    Fall TV content

--- views/widgets/fall-tv/v1.php
+++ views/widgets/fall-tv/v1.php
@@ -10,7 +10,25 @@
 			<div id="wf_widget_by">Brought to you by <span>Womensforum.com </span></div>
 		</div>
 		<div id="wf_widget_content">
-
+            <ul>
+                <li class="alt1">
+                    <img src="/assets/imgs/fall-tv/network-tv-season-starts-up-again.jpg" width="60" height="60" class="pull-left" />
+                    <a href="http://www.womensforum.com/network-tv-season-starts-up-again.html" target="_blank">
+                        Network TV Season Starts Up Again
+                    </a>
+                </li>
+                <li class="alt2"><img src="/assets/imgs/fall-tv/jimmy-fallon-the-tonight-show.jpg" width="60" height="60" class="pull-left" />
+                    <a href="http://www.womensforum.com/the-tonight-show-with-jimmy-fallon-preview.html" target="_blank">
+                        'The Tonight Show' With Jimmy Fallon Preview
+                    </a>
+                </li>
+                <li class="alt3">
+                    <img src="/assets/imgs/fall-tv/snacking-like-the-stars-header-1.jpg" width="60" height="60" class="pull-left" />
+                    <a href="http://www.womensforum.com/snacking-like-the-stars.html" target="_blank">
+                        Snacking Like the Stars
+                    </a>
+                </li>
+            </ul>
 		</div>
 		<div id="wf_widget_footer">
             <script src='http://cdn.onescreen.net/os/static/apps/2.0/_onescreen.js'></script>

4c3df743652f61559e3ca2b7774672823fe3ee80
Ken <KenIovino@gmail.com>
2014-03-25 16:42:44 -0400

    Fall Back Into Fitness Content

--- views/widgets/fall-back-fitness/v1.php
+++ views/widgets/fall-back-fitness/v1.php
@@ -10,7 +10,25 @@
 			<div id="wf_widget_by">Brought to you by <span>Womensforum.com </span></div>
 		</div>
 		<div id="wf_widget_content">
-
+            <ul>
+                <li class="alt1">
+                    <img src="/assets/imgs/fall-back-fitness/burn-calories-in-the-cold-1.jpg" width="60" height="60" class="pull-left" />
+                    <a href="http://www.womensforum.com/shiver-to-shrink.html" target="_blank">
+                        Burn Calories In The Cold
+                    </a>
+                </li>
+                <li class="alt2"><img src="/assets/imgs/fall-back-fitness/lose-weight-without-dieting.jpg" width="60" height="60" class="pull-left" />
+                    <a href="http://www.womensforum.com/lose-weight-without-dieting.html" target="_blank">
+                        Lose Weight Without Dieting
+                    </a>
+                </li>
+                <li class="alt3">
+                    <img src="/assets/imgs/fall-back-fitness/stress-Eating-too-much.gif" width="60" height="60" class="pull-left" />
+                    <a href="http://www.womensforum.com/stress-snacking-got-you-craving-for-more.html" target="_blank">
+                        How to Stop Stress Eating
+                    </a>
+                </li>
+            </ul>
 		</div>
 		<div id="wf_widget_footer">
             <script src='http://cdn.onescreen.net/os/static/apps/2.0/_onescreen.js'></script>

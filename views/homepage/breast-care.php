0d75bebadbbbc63265fa7bce0a79a122bee2780f
Ken <KenIovino@gmail.com>
2014-03-25 15:49:46 -0400

    Removed 'content' text from widget

--- views/homepage/breast-care.php
+++ views/homepage/breast-care.php
@@ -1,11 +1,11 @@
 <div class="col-lg-6 text-center">
-    <h3>Breast Cancer Awareness Content w/ Video + Pre-roll</h3>
+    <h3>Breast Cancer Awareness w/ Video + Pre-roll</h3>
     <script type="text/javascript" src="<?php echo $app->config('widget_js_url'); ?>"></script>
     <div id="wf_widget_v1"></div>
     <script>com.womensforum.widgets.load("2f946e78d544c429926c28992eea9102", "wf_widget_v1");</script>
 </div>
 <div class="col-lg-6 text-center">
-    <h3>Breast Cancer Awareness Content w/ 300x250 Ad</h3>
+    <h3>Breast Cancer Awareness w/ 300x250 Ad</h3>
     <script type="text/javascript" src="<?php echo $app->config('widget_js_url'); ?>"></script>
     <div id="wf_widget_v2"></div>
     <script>com.womensforum.widgets.load("780e8cca5963aa4489ac6bfaf22b415d", "wf_widget_v2");</script>

bf76d89d9eae980a6fbe2609357842ef9a4a8e14
Ken <KenIovino@gmail.com>
2014-03-26 15:39:06 -0400

    Moved 920x66 to the bottom of the page

--- views/homepage/game.php
+++ views/homepage/game.php
@@ -1,22 +1,23 @@
-<div class="col-lg-12 text-center">
-    <h3>Pushdown Game</h3>
+<div class="container">
+    <div class="col-lg-12 text-center">
+        <h3>728x90</h3>
+        <iframe src="http://a.womensforum.com/ads/ddc/slots031414/728x90.html" width="728" height="90" marginwidth="0" marginheight="0" frameborder="0" scrolling="no" frameborder="0" allowtransparency="false"></iframe>
+    </div>
+
+    <div class="col-lg-6 text-center">
+        <h3>300x600</h3>
+        <iframe src="http://a.womensforum.com/ads/ddc/slots031414/300x600.html" width="300" height="600" marginwidth="0" marginheight="0" frameborder="0" scrolling="no" frameborder="0" allowtransparency="false"></iframe>
+    </div>
+
+    <div class="col-lg-6 text-center">
+        <h3>300x250</h3>
+        <iframe src="http://a.womensforum.com/ads/ddc/slots031414/300x250.html" width="300" height="250" marginwidth="0" marginheight="0" frameborder="0" scrolling="no" frameborder="0" allowtransparency="false"></iframe>
+    </div>
 </div>
 
-<div class="col-lg-12 text-center">
-    <iframe src="/assets/imgs/homepage/slots.php" width="1000" height="420" marginwidth="0" marginheight="0" frameborder="0" scrolling="no" frameborder="0" allowtransparency="false"></iframe>
-</div>
-
-<div class="col-lg-12 text-center">
-    <h3>728x90</h3>
-    <iframe src="http://a.womensforum.com/ads/ddc/slots031414/728x90.html" width="728" height="90" marginwidth="0" marginheight="0" frameborder="0" scrolling="no" frameborder="0" allowtransparency="false"></iframe>
-</div>
-
-<div class="col-lg-6 text-center">
-    <h3>300x600</h3>
-    <iframe src="http://a.womensforum.com/ads/ddc/slots031414/300x600.html" width="300" height="600" marginwidth="0" marginheight="0" frameborder="0" scrolling="no" frameborder="0" allowtransparency="false"></iframe>
-</div>
-
-<div class="col-lg-6 text-center">
-    <h3>300x250</h3>
-    <iframe src="http://a.womensforum.com/ads/ddc/slots031414/300x250.html" width="300" height="250" marginwidth="0" marginheight="0" frameborder="0" scrolling="no" frameborder="0" allowtransparency="false"></iframe>
+<div class="container">
+    <div class="col-lg-12 text-center">
+        <h3>970x66</h3>
+        <iframe src="/assets/imgs/homepage/slots.php" width="970" height="420" marginwidth="0" marginheight="0" frameborder="0" scrolling="no" frameborder="0" allowtransparency="false"></iframe>
+    </div>
 </div>

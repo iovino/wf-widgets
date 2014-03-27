2897bef980277d36c5168849686de45047450f64
Ken <KenIovino@gmail.com>
2014-03-27 13:53:06 -0400

    Updated promo badges

--- views/homepage/badges.php
+++ views/homepage/badges.php
@@ -4,7 +4,7 @@
 </div>
 
 <div class="col-lg-12 text-center">
-    <h3>Promo Badge 300x80</h3>
+    <h3>Right Rail Promo Badge</h3>
     <img src="/assets/imgs/homepage/badges/promo_badge_2.png" border="0" />
 </div>
 

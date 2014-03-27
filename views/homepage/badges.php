d8073717baedba5a9a0b1c733f73644d08422a24
Ken <KenIovino@gmail.com>
2014-03-27 14:02:27 -0400

    badge updates

--- views/homepage/badges.php
+++ views/homepage/badges.php
@@ -2,9 +2,10 @@
     <h3>Promo Badge 220x90</h3>
     <img src="/assets/imgs/homepage/badges/promo_badge_1.png" border="0" />
 </div>
-
+<br><br>
 <div class="col-lg-12 text-center">
-    <h3>Right Rail Promo Badge</h3>
+    <h3>Right Rail Promo Badge 300x80</h3>
+    <br><br>
     <img src="/assets/imgs/homepage/badges/promo_badge_2.png" border="0" />
 </div>
 

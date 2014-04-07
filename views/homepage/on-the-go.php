5b2b877ff952266f8840c29fde9b85d583b3d092
Ken <KenIovino@gmail.com>
2014-04-07 10:52:40 -0400

    Added page title

--- views/homepage/on-the-go.php
+++ views/homepage/on-the-go.php
@@ -1,4 +1,5 @@
 <div class="col-lg-12 text-center">
+    <h3>Today On The Go</h3>
     <img src="/assets/imgs/homepage/mobile/WF_MobileScreenCap_1.jpg" border="0" />
 </div>
 <div class="col-lg-12 text-center">

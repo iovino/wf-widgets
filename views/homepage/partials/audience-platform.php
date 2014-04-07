63382546e3da9bd19a025935dc0ba188993580cb
Ken <KenIovino@gmail.com>
2014-04-07 17:26:21 -0400

    Added missing text

--- views/homepage/partials/audience-platform.php
+++ views/homepage/partials/audience-platform.php
@@ -1,12 +1,31 @@
-<div class="col-lg-12 text-center">
-    <h2>Audience Platform Overview</h2>
-    <p style="">Our audience platform captures all behaviors and interactions that make up the highly engaged Womensforum visitor. Our advanced data targeting combines unique first-party data and comprehensive 3rd-party data which allows us to target any segment of visitors with precision and increase our overall reach.  As we leverage frequency and recency, we continually refine our audience segments. The culmination of all these efforts deliver a positive lift in CTR, and greater focus and capacity toward audience targeting, performance, and scale. </p>
+<div class="col-lg-12">
+    <h2 class="text-center">Audience Platform Overview</h2>
+    <p>Our audience platform captures all behaviors and interactions that make up the highly engaged Womensforum visitor. Our advanced data targeting combines unique first-party data and comprehensive 3rd-party data which allows us to target any segment of visitors with precision and increase our overall reach.  As we leverage frequency and recency, we continually refine our audience segments. The culmination of all these efforts deliver a positive lift in CTR, and greater focus and capacity toward audience targeting, performance, and scale. </p>
+    <p>Below are a few of the advanced data segments we utilize to enhance client campaigns:</p>
+    <ul>
+        <li>Past Purchase Behavior</li>
+        <li>Search Intent</li>
+        <li>Content Interest</li>
+        <li>Consumer Actions</li>
+        <li>Social Engagement</li>
+    </ul>
 
+    <p>In addition to these segments, we develop custom segments to identify the exact type of user a client wishes to reach, examples of these segments include:</p>
+    <ul>
+        <li>Fashionistas</li>
+        <li>Deal Seekers</li>
+        <li>Foodies</li>
+        <li>Millennial Moms</li>
+        <li>Influencers</li>
+        <li>Social Mavens</li>
+    </ul>
+</div>
+
+<div class="col-lg-12 text-center">
     <h3>Data Capabilities</h3>
     <img src="/assets/imgs/homepage/audience-platform/data-capabilities.png" border="0" />
 
     <h3>Data Collection &amp; Refinement</h3>
     <img src="/assets/imgs/homepage/audience-platform/data-collection.png" border="0" />
 </div>
-
 
